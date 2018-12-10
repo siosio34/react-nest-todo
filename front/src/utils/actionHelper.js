520957aaeb17d5917d706f5d7154ddc662183675
youngje <siosio3103@gmail.com>
2018-12-10 13:29:47 +0000

    Make action Helper function

--- front/src/utils/actionHelper.js
+++ front/src/utils/actionHelper.js
@@ -1,11 +1,23 @@
-export function makeAsyncActionTypes = {
-    
+const INDEX = 'INDEX';
+const REQUEST = 'REQUEST';
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
+
+export function makeAsyncActionTypes(base) {
+  return [INDEX, REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
+		acc[type] = `${base}_${type}`;
+		return acc;
+	}, {});
 }
 
-export function makeActionCreators = {
-    
+export function makeActionCreator(type, payload = {}) {
+  return {type, ...payload};
 }
 
-export function makeAsyncActionCreators = {
-    
+export function makeAsyncActionCreator(action) {
+	let actionCreator = () => makeActionCreator(action.INDEX);
+	actionCreator.request = () => makeActionCreator(action.REQUEST);
+	actionCreator.success = () => makeActionCreator(action.SUCCESS);
+	actionCreator.failure = () => makeActionCreator(action.FAILURE);
+	return actionCreator;
 }
